d9c6cc4cd77e37b26e88f0cad25959d7cca463c6
abritolda <abritolda@5a50b8e4-c46e-494c-8199-665086288703>
2017-11-28 18:22:42 +0000

    Alteração Cliente FMX
    git-svn-id: http://svn.code.sf.net/p/rest-dataware-componentes/dataware/trunk@1011 5a50b8e4-c46e-494c-8199-665086288703

--- CORE/Demos/FMX/CienteFMX/Unit1.pas
+++ CORE/Demos/FMX/CienteFMX/Unit1.pas
@@ -12,7 +12,8 @@ uses
   FMX.ListBox, FMX.Memo, FireDAC.Stan.Intf, FireDAC.Stan.Option,
   FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf,
   FireDAC.DApt.Intf, FireDAC.Comp.DataSet, FireDAC.Comp.Client, system.diagnostics,
-  System.TimeSpan, IdComponent, uDWConstsData, IdSSLOpenSSL,system.ioutils,IdSSLOpenSSLHeaders;
+  System.TimeSpan, IdComponent, uDWConstsData, IdSSLOpenSSL,system.ioutils,IdSSLOpenSSLHeaders,
+  FMX.TabControl, System.Actions, FMX.ActnList;
 
 type
   TForm1 = class(TForm)
@@ -39,8 +40,34 @@ type
     ListBoxItem3: TListBoxItem;
     StringGrid1: TStringGrid;
     LinkGridToDataSourceBindSourceDB12: TLinkGridToDataSource;
+    tbc1: TTabControl;
+    tablista: TTabItem;
+    RESTDWClientSQL1EMP_NO: TSmallintField;
+    RESTDWClientSQL1FIRST_NAME: TWideStringField;
+    RESTDWClientSQL1LAST_NAME: TWideStringField;
+    RESTDWClientSQL1PHONE_EXT: TWideStringField;
+    RESTDWClientSQL1HIRE_DATE: TSQLTimeStampField;
+    RESTDWClientSQL1DEPT_NO: TWideStringField;
+    RESTDWClientSQL1JOB_CODE: TWideStringField;
+    RESTDWClientSQL1JOB_GRADE: TSmallintField;
+    RESTDWClientSQL1JOB_COUNTRY: TWideStringField;
+    RESTDWClientSQL1SALARY: TFloatField;
+    RESTDWClientSQL1FULL_NAME: TWideStringField;
+    tabaltera: TTabItem;
+    actlst1: TActionList;
+    ChangeTabaltera: TChangeTabAction;
+    ChangeTablista: TChangeTabAction;
+    edtnome: TEdit;
+    edtnome2: TEdit;
+    tlb2: TToolBar;
+    btngrava: TButton;
+    LinkControlToField1: TLinkControlToField;
+    LinkControlToField2: TLinkControlToField;
     procedure btn1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure StringGrid1Tap(Sender: TObject; const Point: TPointF);
+    procedure btngravaClick(Sender: TObject);
+    procedure StringGrid1CellClick(const Column: TColumn; const Row: Integer);
   private
     { Private declarations }
   public
@@ -76,6 +103,18 @@ begin
   listboxitem3.Text:= 'Tempo de Resposta: '+ inttostr(elapsed.Milliseconds)+' milisegundos.';
 end;
 
+procedure TForm1.btngravaClick(Sender: TObject);
+var serro: string;
+begin
+if RESTDWClientSQL1.State in [dsEdit,dsInsert] then
+  RESTDWClientSQL1.Post;
+
+RESTDWClientSQL1.ApplyUpdates(serro);
+if Length(Trim(serro))>0 then
+    showmessage(serro);
+ChangeTablista.Execute;
+end;
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
  {$IF Defined(ANDROID) OR Defined(IOS)}
@@ -91,6 +130,19 @@ begin
  formatsettings.ThousandSeparator:=',';
  RESTDWDataBase1.DecimalSeparator:=formatsettings.DecimalSeparator;
  {$IFEND}
+end;
+
+procedure TForm1.StringGrid1CellClick(const Column: TColumn;
+  const Row: Integer);
+begin
+{$IFDEF  MSWINDOWS}
+ChangeTabaltera.Execute;
+{$ENDIF}
+end;
+
+procedure TForm1.StringGrid1Tap(Sender: TObject; const Point: TPointF);
+begin
+ChangeTabaltera.Execute;
 end;
 
 end.
