6176cf2b0cc608165cda431085dc242441b5083d
Mobius One <26689802+mobius1qwe@users.noreply.github.com>
2022-08-17 16:42:58 -0300

    Project: RESTDataWare Changes:  - Correção de versionamento

--- CORE/Source/Consts/uRESTDWConsts.pas
+++ CORE/Source/Consts/uRESTDWConsts.pas
@@ -70,7 +70,7 @@ Const
  wdays                      : Array [1 .. 7]  Of String = ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'); {do not localize}
  monthnames                 : Array [1 .. 12] Of string = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', {do not localize}
                                                            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'); {do not localize}
- RESTDWVersionINFO          = 'v2.0.0-';
+ RESTDWVersionINFO          = 'v2.0.1-';
  RESTDWRelease              = '3064';
  RESTDWCodeProject          = 'Savage Reign - GitHub';
  RESTDWVersao               = RESTDWVersionINFO + RESTDWRelease + '(' + RESTDWCodeProject + ')';
