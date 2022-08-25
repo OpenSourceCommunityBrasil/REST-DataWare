7866827191f895ebaaad7773e81c3478a01f4749
Mobius One <26689802+mobius1qwe@users.noreply.github.com>
2022-08-25 16:18:33 -0300

    Project: RESTDataWare Changes:  - Atualização de versionamento de branch

--- CORE/Source/Consts/uRESTDWConsts.pas
+++ CORE/Source/Consts/uRESTDWConsts.pas
@@ -70,9 +70,9 @@ Const
  wdays                      : Array [1 .. 7]  Of String = ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'); {do not localize}
  monthnames                 : Array [1 .. 12] Of string = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', {do not localize}
                                                            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'); {do not localize}
- RESTDWVersionINFO          = 'v2.0.4-';
+ RESTDWVersionINFO          = 'v2.1.0-';
  RESTDWRelease              = '3097';
- RESTDWCodeProject          = 'Savage Reign - GitHub';
+ RESTDWCodeProject          = 'Galaga - GitHub';
  RESTDWVersao               = RESTDWVersionINFO + RESTDWRelease + '(' + RESTDWCodeProject + ')';
  GOffsetFromUTC             : TDateTime = 0{$IFDEF HAS_DEPRECATED}deprecated{$ENDIF};
  SCorruptedFileHeader       = 'Corrupted File Header';
