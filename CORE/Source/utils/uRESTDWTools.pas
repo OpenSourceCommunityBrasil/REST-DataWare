b05408983ed27f5b912ed25ad4d40194af44cbd7
Mobius One <26689802+mobius1qwe@users.noreply.github.com>
2022-12-13 13:07:45 -0300

    - Correção de DecodeBase64 para Delphi

--- CORE/Source/utils/uRESTDWTools.pas
+++ CORE/Source/utils/uRESTDWTools.pas
@@ -3164,11 +3164,6 @@ End;
 Function Decode64(const S: string): string;
 Var
  sa : String;
- {$IFNDEF FPC}
-  {$IF CompilerVersion > 27}
-   ne: TBase64Encoding;
-  {$IFEND}
- {$ENDIF}
 Begin
  If (Trim(S) <> '')   And
     (Trim(S) <> '""') Then
@@ -3176,20 +3171,7 @@ Begin
    SA := S;
    If Pos(sLineBreak, SA) > 0 Then
     SA := StringReplace(SA, sLineBreak, '', [rfReplaceAll]);
-   {$IFDEF FPC}
     Result := BytesToString(Base64Decode(SA));
-   {$ELSE}
-    {$IF CompilerVersion > 27}
-     ne     := TBase64Encoding.Create(-1, '');
-     Try
-      Result := ne.Decode(SA);
-     Finally
-      FreeAndNil(ne);
-     End;
-    {$ELSE}
-     Result := BytesToString(Base64Decode(SA));
-    {$IFEND}
-   {$ENDIF}
   End;
 End;
 
