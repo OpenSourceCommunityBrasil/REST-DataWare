e78ded44acc5778d85406aecbc244830559c9c65
Fernando Castelano Banhos <69670357+FernandoBanhos@users.noreply.github.com>
2022-12-23 09:33:43 -0300

    - Troca nos nome dos Arquivos, devido o Delphi incluir automaticamente a unit FireDAC.Phys.RESTDWDef e nao FireDAC.Phys.RDWDef

--- CORE/Source/Database_Drivers/FireDACPhysLink/FireDAC.Phys.RESTDWBase.pas
+++ CORE/Source/Database_Drivers/FireDACPhysLink/FireDAC.Phys.RESTDWBase.pas
@@ -1,4 +1,28 @@
-unit FireDAC.Phys.RDWBase;
+unit FireDAC.Phys.RESTDWBase;
+
+{$I ..\Includes\uRESTDWPlataform.inc}
+
+{
+  REST Dataware .
+  Criado por XyberX (Gilbero Rocha da Silva), o REST Dataware tem como objetivo o uso de REST/JSON
+ de maneira simples, em qualquer Compilador Pascal (Delphi, Lazarus e outros...).
+  O REST Dataware também tem por objetivo levar componentes compatíveis entre o Delphi e outros Compiladores
+ Pascal e com compatibilidade entre sistemas operacionais.
+  Desenvolvido para ser usado de Maneira RAD, o REST Dataware tem como objetivo principal você usuário que precisa
+ de produtividade e flexibilidade para produção de Serviços REST/JSON, simplificando o processo para você programador.
+
+ Membros do Grupo :
+
+ XyberX (Gilberto Rocha)    - Admin - Criador e Administrador  do pacote.
+ Alexandre Abbade           - Admin - Administrador do desenvolvimento de DEMOS, coordenador do Grupo.
+ Anderson Fiori             - Admin - Gerencia de Organização dos Projetos
+ Flávio Motta               - Member Tester and DEMO Developer.
+ Mobius One                 - Devel, Tester and Admin.
+ Gustavo                    - Criptografia and Devel.
+ Eloy                       - Devel.
+ Roniery                    - Devel.
+ Fernando Banhos            - Drivers e Datasets.
+}
 
 interface
 
@@ -6,7 +30,7 @@ uses
   Classes, SysUtils, FireDAC.Phys, FireDAC.Stan.Intf, FireDAC.Phys.Intf,
   FireDAC.Phys.SQLGenerator, FireDAC.Stan.Util, FireDAC.Stan.Param,
   FireDAC.DatS, Firedac.Stan.Option, Variants,
-  uRESTDWBasicDB, DB, uRESTDWPoolermethod, FireDAC.Phys.RDWMeta;
+  uRESTDWBasicDB, DB, uRESTDWPoolermethod, FireDAC.Phys.RESTDWMeta;
 
 type
   TFDPhysRDWConnectionBase = class;
@@ -100,7 +124,7 @@ const
 implementation
 
 uses
-  FireDAC.Phys.RDWDef, Data.SqlTimSt, uRESTDWParams, uRESTDWConsts,
+  FireDAC.Phys.RESTDWDef, Data.SqlTimSt, uRESTDWParams, uRESTDWConsts,
   uRESTDWTools, uRESTDWBasicTypes;
 
 { TFDPhysRDWDriverBase }
