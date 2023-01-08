bde837917c90c2a6c95386c1da110949703a64ff
Mobius One <mobius1qwe@gmail.com>
2023-01-08 17:42:00 -0300

    - Ajuste de versionamento do Trunk

--- CORE/Source/Consts/uRESTDWConsts.pas
+++ CORE/Source/Consts/uRESTDWConsts.pas
@@ -46,7 +46,7 @@ Const
  
  // controle de versão
  RESTDWVersionINFO          = 'v2.0.8-';
- RESTDWRelease              = '25';
+ RESTDWRelease              = '29';
  RESTDWCodeProject          = 'Savage Reign - GitHub';
  RESTDWVersao               = RESTDWVersionINFO + RESTDWRelease + '(' + RESTDWCodeProject + ')';
  RESTDWDialogoTitulo        = 'REST DataWare Components';
