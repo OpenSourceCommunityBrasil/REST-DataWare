18d7745c291a41f8dd7bd8187f3950713b83ed8e
Mobius One <mobius1qwe@gmail.com>
2023-02-01 16:02:21 -0300

    - Ajuste de versionamento do branch

--- CORE/Source/Consts/uRESTDWConsts.pas
+++ CORE/Source/Consts/uRESTDWConsts.pas
@@ -45,7 +45,7 @@ Const
  
  // controle de versão
  RESTDWVersionINFO          = 'v2.1.0-';
- RESTDWRelease              = '371';
+ RESTDWRelease              = '399';
  RESTDWCodeProject          = 'Galaga - GitHub';
  RESTDWVersao               = RESTDWVersionINFO + RESTDWRelease + '(' + RESTDWCodeProject + ')';
  cSetPhysicDriver           = 'A PhysicDriver is needed to work';
